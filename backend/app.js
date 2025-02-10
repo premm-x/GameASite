2bd1586fbcb4000f25f0aa05e409d5b314a05718
premm <mybrother1370@gmail.com>
2025-02-10 22:14:51 +0530

    cors issue

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dbConnection();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'https://battlesfor.onrender.com/',
+    origin: '*',
 }));
 
 
